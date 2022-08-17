42b0bb7133c60dae134a3b7b01ede49e5f07ffd6
R-ocke-R <sharmamanu2727@gmail.com>
2022-08-17 19:43:38 +0530

    Changes

--- SuperBatch/August17/patterns.java
+++ SuperBatch/August17/patterns.java
@@ -28,10 +28,100 @@ public class patterns {
         }
     }
     
+    public static void two(int n){
+        /*
+         * ^ ^ ^ ^ ^
+         * ^       ^
+         * ^       ^
+         * ^       ^
+         * ^ ^ ^ ^ ^
+         */
+        String star="^ ";
+        String space="  ";
+        for (int i = 0; i < n; i++) {
+            
+            if(i==0 || i==n-1){
+                System.out.println(star.repeat(n)); 
+            }
+            else{
+                System.out.print(star);
+                System.out.print(space.repeat(n-2));
+                System.out.println(star);
+            }         
+            
+        }
+    }
+    public static void three(int n){
+        /*
+         *    ^       ^
+         *      ^   ^ 
+         *        ^       
+         *      ^   ^       
+         *    ^       ^
+         */
+        String star="^ ";
+        String space="  ";
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 0; j < n; j++) {
+                if(i==j || i+j==n-1){
+                    System.out.print(star);
+                }
+                else{
+                    System.out.print(space);
+                }
+            }     
+            System.out.println();  
+            
+        }
+    }
+    public static void four(int n){
+        /*
+         *  
+         *     ^  
+         *    ^ ^ 
+         *   ^ ^ ^ 
+         *  ^ ^ ^ ^
+         * ^ ^ ^ ^ ^
+         *  
+         * 
+         * odd even logic
+         */
+    }
+    public static void five(int n){
+        /*
+         * n=5
+         *     |^ ^ ^ ^ ^
+         *     |    ^ ^ ^ ^
+         *     |        ^ ^ ^ 
+         *     |            ^ ^
+         *     |                ^
+         *     |            ^ ^
+         *     |        ^ ^ ^
+         *     |    ^ ^ ^ ^
+         *     |^ ^ ^ ^ ^
+         * 
+         */
+        // char star='^';
+        // char space=' ';
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < i; j++) {
+        //         System.out.print(space+" ");
+        //         System.out.print(space+" ");
+        //     }
+        //     for (int j = i; j < n; j++) {
+        //         System.out.print(star +" ");
+        //     }
+        //     System.out.println();
+        
+        // }
+    }
+    
 
 
 
     public static void main(String[] args) {
-        one(5);
+        three(10);
     }
 }
