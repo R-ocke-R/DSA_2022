0b7bf716ec39dfb4f4045ed11ba1318bdd88e5c0
R-ocke-R <sharmamanu2727@gmail.com>
2022-08-21 23:57:58 +0530

    Linked List Start

---
+++ Love_Babber_Course/Phase_Two/LinkedList/_44_LinkedList.java
@@ -0,0 +1,21 @@
+package Phase_Two.LinkedList;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+    }
+}
+
+public class _44_LinkedList {
+    public static void main(String[] args) {
+        // char ch='a';
+        // ch++;
+        // System.out.println(ch);
+        // for (byte i = 0; i < 128; i++) {
+        //     System.out.println(i);
+        // }
+
+    }
+}
